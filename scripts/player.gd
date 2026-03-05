e938691246c76ae39d1fbce5af6c6aea492ff154
SYnster000 <vinicius.felipe.rocha@gmail.com>
2026-03-05 14:24:14 -0300

    05

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 200.0
 const JUMP_FORCE = -400.0
 
+var is_jumping := false
+@onready var animation := $anim as AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		is_jumping = true
+	else:
+		is_jumping = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
@@ -19,7 +24,16 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		animation.scale.x = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()
+	
+	#controle de animações
+	if is_jumping:
+		animation.play("jump")
+	elif direction:
+		animation.play("run")
+	else:
+		animation.play("idle")
+	
